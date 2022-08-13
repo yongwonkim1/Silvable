5247eaea58062ed20ee3464dc171f57ea9d522cc
yongwonkim1 <qwsa522@naver.com>
2022-08-13 21:04:27 +0900

    삭제추가

--- Memo/MemoDetail.js
+++ Memo/MemoDetail.js
@@ -1,50 +1,50 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { StyleSheet, Text, View, Button,Pressable,TextInput } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import { UserContext } from "../contexts";
+import { userCollection } from '../lib/user';
 
 const MemoDetail = ({route, navigation}) => {
     const userEmail = useContext(UserContext);
     const email = userEmail.user.email;
     const uid = userEmail.user.uid;
-    const usersCollection = firestore().collection('memo');
     const [title,setTitle] = useState(route.params?.title);
     const [text,setText] = useState(route.params?.text);
-
+    const addCollection = firestore().collection('memo');
     const UpdateDB = async () => {
-        try {
-          const rows = await usersCollection.where('title', '==',title, "AND", "email","==",email);
-          rows.get().then(function (querySnapshot) {
-            querySnapshot.forEach(function (doc) {
-              doc.update({ title: title, text:text });
-            });
+      try {
+        const rows = await addCollection.where('title', '==',title);
+        rows.get().then(function (querySnapshot) {
+          querySnapshot.forEach(function (doc) {
+            doc.ref.update({ title : title, text:text});
+            console.log(title,text)
           });
-          console.log('Update Complete!', rows);
-        } catch (error) {
-          console.log(error.message);
-        }
-      };
-      const addCollection = firestore().collection('users');
-      const DeleteDB = async () => {
-        try {
-          //   await addCollection.doc('').delete();
-          const rows = await addCollection.where('title', '==', title);
-          rows.get().then(function (querySnapshot) {
-            querySnapshot.forEach(function (doc) {
-              doc.ref.delete();
-            });
+        });
+        console.log('Update Complete!', rows);
+      } catch (error) {
+        console.log(error.message);
+      }}
+    
+    const DeleteDB = async () => {
+      try {
+        //   await addCollection.doc('').delete();
+        const rows = await addCollection.where('title', '==',title);
+        rows.get().then(function (querySnapshot) {
+          querySnapshot.forEach(function (doc) {
+            doc.ref.delete();
           });
-          console.log('Delete Complete!', rows);
-        } catch (error) {
-          console.log(error.message);
-        }
-      };
+        });
+        console.log('Delete Complete!', rows);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
 
     return(
         <View>
             <TextInput value={title} onChangeText={setTitle}/>
             <TextInput value={text} onChangeText={setText}/>
-            <Button onPress={UpdateDB} title="수정"/><Button onPress={DeleteDB} title="삭제"/>
+            <Button  onPress={UpdateDB} title="수정"/><Button onPress={DeleteDB} title="삭제"/>
 
         </View>
     );
