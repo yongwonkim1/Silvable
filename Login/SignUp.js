3272b30e15dc276edc3473d40ca3cb204547837a
Kim Junha laptop <kim_jh6859@naver.com>
2022-08-14 19:43:41 +0900

    login merge

--- Login/SignUp.js
+++ Login/SignUp.js
@@ -1,15 +1,17 @@
 import React, { useState, useRef, useEffect, useContext } from 'react';
-import { Text} from 'react-native';
+import { Text } from 'react-native';
 import styled from 'styled-components/native';
 import { Image, Input, Button } from '../Components';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { validateEmail, removeWhitespace } from '../utils/common';
 import { images } from '../utils/images';
 import { useNavigation } from '@react-navigation/native';
+import firestore from '@react-native-firebase/firestore';
 
 //파이어베이스 회원가입 
 import { Alert } from 'react-native';
 import { signUp } from '../lib/auth';
+import { Firestore } from 'firebase/firestore';
 // import { dbService } from '../utils/firebase';
 // import { addDoc, collection } from 'firebase/firestore';
 
@@ -34,46 +36,52 @@ const ErrorText = styled.Text`
 
 
 
-const SignUp = ( ) => {
+const SignUp = () => {
 
   const navigation = useNavigation();
-  
+
   const [email, setEmail] = useState('');
+  const [email2, setEmail2] = useState('');
   const [password, setPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [disabled, setDisabled] = useState(true);
 
   const emailRef = useRef();
+  const email2Ref = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
 
   //회원가입시에 이름을 안입력했을 경우, 이메일 형식에 맞지 않는 이메일을 입력했을 경우, 비밀번호 최소 수를 넘지 않을 경우, 일치하지 않을 경우
   useEffect(() => {
-      let _errorMessage = '';
-      if (!validateEmail(email)) {
-        _errorMessage = '이메일을 입력해주세요';
-      } 
+    let _errorMessage = '';
+    if (!validateEmail(email)) {
+      _errorMessage = '이메일을 입력해주세요';
+    }
 
-      else if (password.length < 8) {
-        _errorMessage = '비밀번호는 최소 8자 이상이어야 합니다';
-      } 
-      
-      else if (password !== passwordConfirm) {
-        _errorMessage = '비밀번호가 일치하지 않습니다';
-      } 
-      
-      else {
-        _errorMessage = '';
-      }
-      setErrorMessage(_errorMessage);
-    } , [email, password, passwordConfirm]);
+    if (!validateEmail(email2)) {
+      _errorMessage = '보호자,노인 이메일을 입력해주세요(필수x)';
+    }
+
+    else if (password.length < 8) {
+      _errorMessage = '비밀번호는 최소 8자 이상이어야 합니다';
+    }
+
+    else if (password !== passwordConfirm) {
+      _errorMessage = '비밀번호가 일치하지 않습니다';
+    }
+
+    else {
+      _errorMessage = '';
+    }
+    setErrorMessage(_errorMessage);
+  }, [email, email2, password, passwordConfirm]);
 
   useEffect(() => {
     setDisabled(
-      !(email && password && passwordConfirm && !errorMessage)
+      !(email && email2 && password && passwordConfirm && !errorMessage)
     );
-  }, [email, password, passwordConfirm, errorMessage]);
+  }, [email, email2, password, passwordConfirm, errorMessage]);
 
   // const _handleSignupButtonPress = async() => {
   //   try{
@@ -93,22 +101,36 @@ const SignUp = ( ) => {
   const signUpSubmit = async () => { // 회원가입 함수
     const info = { email, password };
     try {
-        const { user } = await signUp(info);
-        console.log(user);
-        navigation.navigate("Login")
+      const { user } = await signUp(info);
+      console.log(user);
+      navigation.navigate("Login")
+      await firestore().collection("users").add({
+        signupCreatedAt: Date.now(),
+        creatorId: user.uid,
+        secondId: email2,
+      })
     } catch (e) {
-        Alert.alert("회원가입에 실패하였습니다.");
-    }}
+      Alert.alert("회원가입에 실패하였습니다.");
+    }
+  }
 
   //화면 스크롤,, 잘은 모르지만 이렇게 하는거래서 함 ㅎㅎ
   return (
-    <KeyboardAwareScrollView> 
+    <KeyboardAwareScrollView>
       <Container>
         <Input ref={emailRef} label="Email"
           value={email}
           onChangeText={text => setEmail(removeWhitespace(text))}
           onSubmitEditing={() => passwordRef.current.focus()}
           placeholder="이메일"
+          returnKeyType="next"
+        />
+
+        <Input ref={email2Ref} label="Email"
+          value={email2}
+          onChangeText={text => setEmail2(removeWhitespace(text))}
+          onSubmitEditing={() => passwordRef.current.focus()}
+          placeholder="노인/보호자 이메일"
           returnKeyType="next"
         />
 
