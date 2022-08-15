347e69111eee482bb4e121f2b6142344431c3188
Kim Junha <kim_jh6859@naver.com>
2022-08-16 00:31:23 +0900

    memodatail.js master

--- Memo/MemoDetail.js
+++ Memo/MemoDetail.js
@@ -1,76 +1,76 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { StyleSheet, Text, View, Button,Pressable,TextInput, Alert } from 'react-native';
+import { StyleSheet, Text, View, Button, Pressable, TextInput, Alert } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import { UserContext } from "../contexts";
 import { userCollection } from '../lib/user';
 import { useNavigation } from '@react-navigation/native';
 import { async } from '@firebase/util';
 
-const MemoDetail = ({route, navigation}) => {
-    const userEmail = useContext(UserContext);
-    const email = userEmail.user.email;
-    const uid = userEmail.user.uid;
-    const [title,setTitle] = useState(route.params?.title);
-    const [text,setText] = useState(route.params?.text);
-    const [contentEmail, setContentEmail] = useState(route.params?.email);
-    const [id,setId] = useState(route.params?.id);
-    const [isOwner, setIsOwner] = useState(false);
+const MemoDetail = ({ route, navigation }) => {
+  const userEmail = useContext(UserContext);
+  const email = userEmail.user.email;
+  const uid = userEmail.user.uid;
+  const [title, setTitle] = useState(route.params?.title);
+  const [text, setText] = useState(route.params?.text);
+  const [contentEmail, setContentEmail] = useState(route.params?.email);
+  const [id, setId] = useState(route.params?.id);
+  const [isOwner, setIsOwner] = useState(false);
 
-    useEffect(()=>{
-      if(contentEmail == email){
-        setIsOwner(true);
-      }
-    },[])
-
-
-    const addCollection = firestore().collection('memo');
-
-    
-
-    const navigation2 = useNavigation();
-
-    
-    const UpdateDB = async () => {
-      await firestore().collection("memo").doc(id).update({
-        title: title,
-        text: text,
-      })
-
-      naviGo();
+  useEffect(() => {
+    if (contentEmail == email) {
+      setIsOwner(true);
     }
+  }, [])
 
-    const naviGo = () => {
-      navigation2.navigate("MemoView");
-    }
-    
-    const DeleteDB = async () => {
-      
-      console.log("삭제버튼 클릭");
-      await firestore().collection("memo").doc(id).delete();
-      naviGo();
-      
-    };
 
-    const alertDelete = () => {
-      Alert.alert("삭제","메모를 삭제할까요?",[{
-          text: "아니요",
-          onPress:  ()=>{},
-          style: "cancel",
-      },{
-          text: "예",
-          onPress: ()=>{DeleteDB()},
-      }],
-      {cancelable: false, onDismiss: ()=>{}});
-      //await deleteDoc(doc(dbService, 'Memos', memoObj.id));
+  const addCollection = firestore().collection('memo');
+
+
+
+  const navigation2 = useNavigation();
+
+
+  const UpdateDB = async () => {
+    await firestore().collection("memo").doc(id).update({
+      title: title,
+      text: text,
+    })
+
+    naviGo();
   }
 
-    return(
-        <View>
-            <TextInput value={title} onChangeText={setTitle}/>
-            <TextInput value={text} onChangeText={setText}/>
-            {isOwner ? <View><Button  onPress={UpdateDB} title="수정"/><Button onPress={alertDelete} title="삭제"/></View> : null}
+  const naviGo = () => {
+    navigation2.navigate("MemoView");
+  }
 
-        </View>
-    );
+  const DeleteDB = async () => {
+
+    console.log("삭제버튼 클릭");
+    await firestore().collection("memo").doc(id).delete();
+    naviGo();
+
+  };
+
+  const alertDelete = () => {
+    Alert.alert("삭제", "메모를 삭제할까요?", [{
+      text: "아니요",
+      onPress: () => { },
+      style: "cancel",
+    }, {
+      text: "예",
+      onPress: () => { DeleteDB() },
+    }],
+      { cancelable: false, onDismiss: () => { } });
+    //await deleteDoc(doc(dbService, 'Memos', memoObj.id));
+  }
+
+  return (
+    <View>
+      <TextInput value={title} onChangeText={setTitle} />
+      <TextInput value={text} onChangeText={setText} />
+      {isOwner ? <View><Button onPress={UpdateDB} title="수정" /><Button onPress={alertDelete} title="삭제" /></View> : null}
+
+    </View>
+  );
 }
 export default MemoDetail;
